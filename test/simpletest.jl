fa4eea32f2d0ed48c36998f218717124537db5d4
Jonathan A. White <jawhite@caltech.edu>
2022-01-11 14:54:31 -0800

    add branching test

--- test/simpletest.jl
+++ test/simpletest.jl
@@ -1,9 +1,10 @@
 using DNASeqFISHChromosomeAssignment
 using DataFrames
+using Test
 
 # make simple test "chromosome"
 
-df = DataFrame(
+main = DataFrame(
     Dict(
         "fov" => fill(1,11),
         "cellID" => fill(1,11),
@@ -15,4 +16,23 @@ df = DataFrame(
     )
 )
 
-res = assign_chromosomes(df, 3, 1.5, 2, 5)
+res1 = assign_chromosomes(main, 5.0, 1.5, 2, 2, false)
+
+res1_alleles = unique(res1.ldp_allele)
+@test length(res1_alleles) == 1 && -1 ∉ res1_alleles
+
+branch = DataFrame(
+    Dict(
+        "fov" => fill(1,6),
+        "cellID" => fill(1,6),
+        "chrom" => fill(1,6),
+        "x" => Array(6.0:11.0),
+        "y" => fill(14, 6),
+        "z" => fill(1.0,6),
+        "g" => Array(6:11),
+    )
+)
+
+branched = vcat(main, branch)
+
+res2 = assign_chromosomes(branched, 5.0, 1.5, 2, 2, false)
