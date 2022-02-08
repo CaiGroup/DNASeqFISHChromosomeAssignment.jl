5963e76abfc8b4bc5e17210a332b4877bc56bf08
jonat <jawhite@caltech.edu>
2022-02-08 09:05:52 -0800

    add Statistics dep

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,8 +16,13 @@ max_paths = 2
 min_size=100
 min_prop_unique=0.9
 dbscan_min_pnts = 100
+prms = ChromSepParams()
+set_min_size(prms, 100)
+set_r_ldp(prms, 1500)
+set_r_dbscan(prms,1500)
+set_sigma(prms, 750)
 #res = assign_chromosomes(far_chrms, r, sig, max_paths)
-res = assign_chromosomes(far_chrms, r, r, sig, r)#, optimizer=CPLEX.Optimizer)
+res = assign_chromosomes(far_chrms, prms)#r, r, sig, r)#, optimizer=CPLEX.Optimizer)
 #ldp_final_eq = (res.ldp_allele .== res.final_allele)
 dbscan_final_eq = (res.dbscan_allele .== res.dbscan_ldp_nbr_allele)
 
@@ -31,7 +36,7 @@ dbscan_final_eq = (res.dbscan_allele .== res.dbscan_ldp_nbr_allele)
 end
 
 #res = assign_chromosomes(close_chrms, r, sig, max_paths)
-res = assign_chromosomes(close_chrms, r, r, sig, r)#, min_size, min_prop_unique, dbscan_min_pnts)#, CPLEX.Optimizer)
+res = assign_chromosomes(close_chrms, prms)#r, r, sig, r)#, min_size, min_prop_unique, dbscan_min_pnts)#, CPLEX.Optimizer)
 
 
 @testset "Close Chromosomes" begin
